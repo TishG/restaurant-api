15b6d1f702791d951dac013249ba24d394901f3d
Latisha <latisha.griffiths@gmail.com>
2019-02-05 16:53:53 -0600

    testing api calls for heroku

--- src/controllers/resultsController.js
+++ src/controllers/resultsController.js
@@ -3,19 +3,26 @@ const zomatoUserKey = process.env.zomatoUserKey;
 
 module.exports = {
     results(req, res, next) {
-      const entry = req.query.entry;
-      const entity = req.query.entity;
-      const options = { 
-      url: `https://developers.zomato.com/api/v2.1/search?q=entity_${entity}%3D${entry}%3D`,
-      headers: 
-       { 'cache-control': 'no-cache',
-         'user-key': zomatoUserKey }
-        };
-      request(options, (err, response, body) => {
-          if(!err && response.statusCode == 200) {
-              const data = JSON.parse(body);
-              res.render('results', {data: data});
-          }
-      })
+      res.render('results', {data: 
+        {restaurants: [
+          {restaurant: {name: "Way 2 Cheezy", location: {address: "404 imaginary drive"}}},
+          {restaurant: {name: "Pizza and things", location: {address: "405 imaginary drive"}}}
+        ]
+      }
+    });
+      // const entry = req.query.entry;
+      // const entity = req.query.entity;
+      // const options = { 
+      // url: `https://developers.zomato.com/api/v2.1/search?q=entity_${entity}%3D${entry}%3D`,
+      // headers: 
+      //  { 'cache-control': 'no-cache',
+      //    'user-key': zomatoUserKey }
+      //   };
+      // request(options, (err, response, body) => {
+      //     if(!err && response.statusCode == 200) {
+      //         const data = JSON.parse(body);
+      //         res.render('results', {data: data});
+      //     }
+      // })
     }
   }
